752d26e07e87350c36b78225095868b0dd3fe3b4
Rodrigo Ourique <rodrigo.guima.ourique@gmail.com>
2024-08-10 19:06:29 -0300

    Finally building

--- src/tree.rs
+++ src/tree.rs
@@ -1,16 +1,16 @@
 // Simple non-binary tree structure
-pub struct TreeNode<'a> {
+pub struct TreeNode {
     pub ft_index: Option<i32>, // index of file table
     pub value: char,           // later we need to support patricia trees
-    pub children: [Option<&'a TreeNode<'a>>; 26],
+    pub children: [Option<Box<TreeNode>>; 26],
 }
 
 // Tree-related debug functions
 pub fn print_tree(tree: &TreeNode) {
     println!("{}", tree.value);
-    for &child in &tree.children {
+    for child in tree.children.iter() {
         if child.is_some() {
-            print_tree(child.unwrap());
+            print_tree(child.as_ref().unwrap());
         }
     }
 }
--- src/trie.rs
+++ src/trie.rs
@@ -1,22 +1,42 @@
 use crate::tree::*;
 
-pub fn search_trie<'a>(root: &'a TreeNode<'a>, key: String) -> Option<&'a TreeNode<'a>> {
-    if key == "" { //string expired
+pub fn search_trie<'a>(root: &'a TreeNode, key: &str) -> Option<&'a TreeNode> {
+    if key.is_empty() {
         return Some(root);
     }
     else {
         let desired_index = root.value as usize - 'a' as usize;
-        let next_node = root.children[desired_index];
-        let next_key = key[1..].to_string();
+        let next_node = &root.children[desired_index];
 
-        if next_node.is_none(){
-            return None;
-        }
-        else {
-            return search_trie(next_node.unwrap(), next_key);
+        match next_node {
+            Some(ref node) => search_trie(node, &key[1..]),
+            None => None,
         }
     }
 }
 
-pub fn insert_trie(root: &mut TreeNode, node: &mut TreeNode) {
+pub fn insert_trie(root: &mut TreeNode, key: String, ft_index: i32) {
+    let desired_index = key.chars().next().unwrap() as usize - 'a' as usize;
+    let next_key = key[1..].to_string();
+    let next_node = &root.children[desired_index];
+    const ARRAY_REPEAT_VALUE: Option<Box<TreeNode>> = None; //Typing of this is needed in const so that rust compiler does not go crazy when initing our pointer arr structs.
+
+    //will nuke existing node if existing (revise later)
+    if next_key.len() == 0 {
+        root.children[desired_index] = Some(Box::new(TreeNode {
+            ft_index: Some(ft_index),
+            value: key.chars().next().unwrap(),
+            children: [ARRAY_REPEAT_VALUE; 26],
+        }));
+    }
+    else {
+        if next_node.is_none() {
+            root.children[desired_index] = Some(Box::new(TreeNode {
+                ft_index: None,
+                value: key.chars().next().unwrap(),
+                children: [ARRAY_REPEAT_VALUE; 26],
+            }));
+        }
+        insert_trie(root.children[desired_index].as_mut().unwrap(), next_key, ft_index);
+    }
 }
