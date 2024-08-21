9bf0a1c6cfd7412ce670eef1991ad2ebf1df248d
Rodrigo Ourique <rodrigo.guima.ourique@gmail.com>
2024-08-21 09:29:44 -0300

    temp

--- src/tree.rs
+++ src/tree.rs
@@ -4,11 +4,13 @@ use std::io::{Read, Write};
 // Simple non-binary tree structure
 // pub const TREE_NODE_CHILD: Option<Box<TreeNode>> = None; //Typing of this is needed in const so that rust compiler does not go crazy when initing our pointer arr structs.
 // pub const TREE_NODE_CHILD_LENGTH: usize = 255; //supporting only ascii for now
-pub const TREE_NODE_VALUE_SIZE: usize = 128; //supporting only ascii for now
+pub const TREE_NODE_VALUE_SIZE: usize = 64; //supporting only ascii for now
+pub const TREE_NODE_PATH_SIZE: usize = 255; //supporting only ascii for now
 
 // binary trie
 pub struct TreeNode {
     pub value: [u8; TREE_NODE_VALUE_SIZE], //will also work with patricia trees. this string will later be compreesed if there is time
+    pub path: [u8; TREE_NODE_PATH_SIZE], //path to file the node references
 }
 
 // Tree-related debug functions
@@ -43,17 +45,25 @@ pub fn read_tree_from_file (path: &str) -> Vec<TreeNode>{
     let mut byte_count = 0;
     let mut tree: Vec<TreeNode> = Vec::new();
     file.bytes().for_each(|byte| {
-        if byte_count == TREE_NODE_VALUE_SIZE {
+        if byte_count == TREE_NODE_VALUE_SIZE  + TREE_NODE_PATH_SIZE {
             byte_count = 0;
         }
         if byte_count == 0 {
             tree.push(TreeNode {
-                value: [0; 128],
+                value: [0; TREE_NODE_VALUE_SIZE],
+                path: [0; TREE_NODE_PATH_SIZE],
             });
         }
-        //push byte to vec
-        let current_length = tree.len();
-        tree[current_length - 1].value[byte_count] = byte.unwrap();
+
+        //push byte to node
+        let current_index = tree.len() - 1;
+        if byte_count < TREE_NODE_VALUE_SIZE {
+            tree[current_index].value[byte_count] = byte.unwrap();
+        }
+        else {
+            tree[current_index].path[byte_count] = byte.unwrap();
+        }
+
         byte_count += 1;
     });
     return tree;
--- src/trie.rs
+++ src/trie.rs
@@ -1,45 +1,26 @@
 use crate::tree::*;
 
-pub fn search_trie<'a>(root: &'a TreeNode, key: &str) -> Option<&'a TreeNode> {
-    if key.is_empty() {
-        return Some(root);
-    } else {
-        let desired_index = key.chars().next().unwrap() as usize;
-        let next_node = &root.children[desired_index];
-        match next_node {
-            Some(ref node) => {
-                //println!("Next node: {}", node.value);
-                return search_trie(node, &key[1..]);
-            }
-            None => return None,
+pub fn search_trie(tree: Vec<TreeNode>, key: &str) -> Option<String> {
+    let mut found_node: Option<&TreeNode> = None;
+    let mut key_bit_index = 0;
+    let mut vec_index =  0;
+    while found_node.is_none() {
+        let str_number = *key & 2 << key_bit_index;
+        let current_node = &tree[vec_index];
+        if current_node.value == key.as_bytes() {
+            found_node = Some(current_node);
+        } else {
+            let left_child = 2 * vec_index + 1;
+            let right_child = 2 * vec_index + 2;
+            //TODO
         }
+    }
+    if found_node.is_none() {
+        return None;
+    } else {
+        return found_node.map(|node| String::from_utf8(node.value.to_vec()).unwrap()); //u8 array to vec to string. optimize if there is time
     }
 }
 
-pub fn insert_trie(root: &mut TreeNode, key: String, ft_index: i32) {
-    let desired_index = key.chars().next().unwrap() as usize;
-    let next_key = key[1..].to_string();
-    let next_node = &root.children[desired_index];
-
-    //will nuke existing node if existing (revise later)
-    if next_key.len() == 0 {
-        root.children[desired_index] = Some(Box::new(TreeNode {
-            ft_index: Some(ft_index),
-            value: key.chars().next().unwrap(),
-            children: [TREE_NODE_CHILD; TREE_NODE_CHILD_LENGTH],
-        }));
-    } else {
-        if next_node.is_none() {
-            root.children[desired_index] = Some(Box::new(TreeNode {
-                ft_index: None,
-                value: key.chars().next().unwrap(),
-                children: [TREE_NODE_CHILD; TREE_NODE_CHILD_LENGTH],
-            }));
-        }
-        insert_trie(
-            root.children[desired_index].as_mut().unwrap(),
-            next_key,
-            ft_index,
-        );
-    }
+pub fn insert_trie(tree: Vec<TreeNode>, key: String) {
 }
