846ba811c1de81cfcb4424b2cfae7c188cb782aa
Rodrigo Guimarães Ourique <rodrigo.guima.ourique@gmail.com>
2024-08-22 10:49:03 -0300

    temp2

--- src/tree.rs
+++ src/tree.rs
@@ -1,4 +1,3 @@
-
 use std::io::{Read, Write};
 
 // Simple non-binary tree structure
@@ -10,11 +9,11 @@ pub const TREE_NODE_PATH_SIZE: usize = 255; //supporting only ascii for now
 // binary trie
 pub struct TreeNode {
     pub value: [u8; TREE_NODE_VALUE_SIZE], //will also work with patricia trees. this string will later be compreesed if there is time
-    pub path: [u8; TREE_NODE_PATH_SIZE], //path to file the node references
+    pub path: [u8; TREE_NODE_PATH_SIZE],   //path to file the node references
 }
 
 // Tree-related debug functions
-pub fn print_tree(tree: Vec<TreeNode>, index: usize) {
+pub fn print_tree(tree: &Vec<TreeNode>, index: usize) {
     let tree_length = tree.len();
     let left_child = 2 * index + 1;
     let right_child = 2 * index + 2;
@@ -26,7 +25,7 @@ pub fn print_tree(tree: Vec<TreeNode>, index: usize) {
     }
 }
 
-pub fn write_tree_to_file(tree: Vec<TreeNode>, path: &str){
+pub fn write_tree_to_file(tree: Vec<TreeNode>, path: &str) {
     let mut file = match std::fs::File::create(&path) {
         Ok(file) => file,
         Err(e) => panic!("Failed to create file: {}", e),
@@ -36,7 +35,7 @@ pub fn write_tree_to_file(tree: Vec<TreeNode>, path: &str){
     }
 }
 
-pub fn read_tree_from_file (path: &str) -> Vec<TreeNode>{
+pub fn read_tree_from_file(path: &str) -> Vec<TreeNode> {
     let mut file = match std::fs::File::open(path) {
         Ok(file) => file,
         Err(e) => panic!("Failed to open file: {}", e),
@@ -45,7 +44,7 @@ pub fn read_tree_from_file (path: &str) -> Vec<TreeNode>{
     let mut byte_count = 0;
     let mut tree: Vec<TreeNode> = Vec::new();
     file.bytes().for_each(|byte| {
-        if byte_count == TREE_NODE_VALUE_SIZE  + TREE_NODE_PATH_SIZE {
+        if byte_count == TREE_NODE_VALUE_SIZE + TREE_NODE_PATH_SIZE {
             byte_count = 0;
         }
         if byte_count == 0 {
@@ -59,8 +58,7 @@ pub fn read_tree_from_file (path: &str) -> Vec<TreeNode>{
         let current_index = tree.len() - 1;
         if byte_count < TREE_NODE_VALUE_SIZE {
             tree[current_index].value[byte_count] = byte.unwrap();
-        }
-        else {
+        } else {
             tree[current_index].path[byte_count] = byte.unwrap();
         }
 
--- src/trie.rs
+++ src/trie.rs
@@ -1,26 +1,47 @@
+use std::{string, thread::current};
+
 use crate::tree::*;
 
-pub fn search_trie(tree: Vec<TreeNode>, key: &str) -> Option<String> {
+// not perfect
+pub fn search_trie(tree: Vec<TreeNode>, key: &str) -> (Option<String>, usize) {
     let mut found_node: Option<&TreeNode> = None;
     let mut key_bit_index = 0;
-    let mut vec_index =  0;
+    let mut vec_index = 0;
     while found_node.is_none() {
-        let str_number = *key & 2 << key_bit_index;
+        if vec_index >= tree.len() {
+            break;
+        }
+        let str_number = key.as_bytes()[key_bit_index];
         let current_node = &tree[vec_index];
-        if current_node.value == key.as_bytes() {
+        let left_child = 2 * vec_index + 1;
+        let right_child = 2 * vec_index + 2;
+
+        if String::from_utf8(current_node.value.to_vec())
+            .unwrap()
+            .contains(key)
+            &&
+        {
+            //change this can be prefix search therefore not useful
             found_node = Some(current_node);
         } else {
-            let left_child = 2 * vec_index + 1;
-            let right_child = 2 * vec_index + 2;
-            //TODO
+            if str_number != 0 {
+                vec_index = right_child
+            } else {
+                vec_index = left_child
+            }
         }
+        key_bit_index += 1;
     }
+
+    //decide what to return
     if found_node.is_none() {
-        return None;
+        return (None, 999);
     } else {
-        return found_node.map(|node| String::from_utf8(node.value.to_vec()).unwrap()); //u8 array to vec to string. optimize if there is time
+        return (
+            Some(String::from_utf8(found_node.unwrap().path.to_vec()).unwrap()),
+            vec_index,
+        );
     }
 }
 
-pub fn insert_trie(tree: Vec<TreeNode>, key: String) {
-}
+pub fn insert_trie(tree: Vec<TreeNode>, key: String) {}
