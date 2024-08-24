6f01957053a304b57269fad6820bd41fdfda7175
Rodrigo Ourique <rodrigo.guima.ourique@gmail.com>
2024-08-23 23:24:02 -0300

    ammend

--- src/tree.rs
+++ src/tree.rs
@@ -7,6 +7,7 @@ pub const TREE_NODE_VALUE_SIZE: usize = 64; //supporting only ascii for now
 pub const TREE_NODE_PATH_SIZE: usize = 255; //supporting only ascii for now
 
 // binary trie
+#[derive(Clone)]
 pub struct TreeNode {
     pub value: [u8; TREE_NODE_VALUE_SIZE], //will also work with patricia trees. this string will later be compreesed if there is time
     pub path: [u8; TREE_NODE_PATH_SIZE],   //path to file the node references
@@ -17,6 +18,14 @@ pub fn print_tree(tree: &Vec<TreeNode>, index: usize) {
     let tree_length = tree.len();
     let left_child = 2 * index + 1;
     let right_child = 2 * index + 2;
+
+    println!(
+        "Index: {}, Value: {:?}, Path: {:?}",
+        index,
+        tree[index].value,
+        tree[index].path
+    );
+
     if left_child < tree_length {
         print_tree(tree, left_child);
     }
